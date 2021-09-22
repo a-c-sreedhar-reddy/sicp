84ea13589e11fb01688861329eefd03467e04b05
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2021-09-23 00:32:41 +0530

    add solutions

---
+++ solutions/ex_1_44.rkt
@@ -0,0 +1,16 @@
+
+#lang racket 
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= n 1) f (compose f (repeated f (- n 1)))))
+
+(define dx 0.0001)
+
+(define (smooth f)
+  (lambda (x) (/ (+ (f (- x dx)) (f x) (f (+ x dx))) 3)))
+
+(define (n-fold-smooth f n)
+  (repeated (smooth f) n))
