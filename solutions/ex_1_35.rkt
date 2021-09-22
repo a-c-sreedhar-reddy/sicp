b1d932ea92feb9a90183dccf001d888f416ee3a1
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2021-09-22 10:04:27 +0530

    solution

---
+++ solutions/ex_1_35.rkt
@@ -0,0 +1,21 @@
+#lang racket
+; fixed point of the transformation x => 1 + 1/x is a number which satisfies
+; x = 1 + 1/x
+; => x^2 = x + 1
+; golden ratio satisfies the above equation.
+; so golden ratio is the fixed point of the above transformation.
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+
+(fixed-point (lambda (x) (+ 1 (/ 1.0 x))) 1)
+;1.6180327868852458
