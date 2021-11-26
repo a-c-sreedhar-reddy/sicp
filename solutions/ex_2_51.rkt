c3b1954e15018cc3c005d06bf041fb0f418cc35a
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2021-11-26 08:36:06 +0530

    add solutions

---
+++ solutions/ex_2_51.rkt
@@ -0,0 +1,58 @@
+#lang racket
+(define (beside painter1 painter2)
+  (let ((split-point (make-vect 0.5 0.0)))
+    (let ((paint-left
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 0.0 1.0)))
+          (paint-right
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.5 1.0))))
+      (lambda (frame)
+        (paint-left frame)
+        (paint-right frame)))))
+
+
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-top
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.0)
+                              (make-vect 0.0 1.0)))
+          (paint-left
+           (transform-painter painter2
+                              (make-vect 0.0 0.0)
+                              (make-vect 1.0 0.0)
+                              split-point)))
+      (lambda (frame)
+        (paint-top frame)
+        (paint-bottom frame)))))
+
+
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 0 1)
+                     (make-vect 1 1)
+                     (make-vect 0 0)))
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vect 1 0)
+                     (make-vect 1 1)
+                     (make-vect 0 0)))
+
+(define (rotate180 painter)
+  ((repeat rotate90 2) painter))
+
+(define (rotate270 painter)
+  ((repeat rotate90 3) painter))
+
+
+(define (below-with-rotate painter1 painter2)
+  (lambda (frame)
+    (rotate90 (beside (rotate270 painter2) (rotate270 painter1)))))
+  
