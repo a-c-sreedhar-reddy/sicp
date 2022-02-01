9d5f4ff7c8a8741b10d21ffb260be2efd09c6c52
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2022-02-01 08:05:40 +0530

    2.59

---
+++ solutions/ex_02_59.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (cons (car set1)
+                    (union-set (cdr set1) set2)))))
+
+(define set1 (list 1 2 3 4))
+
+(define set2  (list 1 4 7 9))
+
+(union-set set1 set2)
