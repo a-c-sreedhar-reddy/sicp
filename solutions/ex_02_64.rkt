5982638821ae1aac8376040a4102b536b516d7c3
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2022-02-13 16:04:18 +0530

    2.64

---
+++ solutions/ex_02_64.rkt
@@ -0,0 +1,10 @@
+; partial-tree takes a list and recursively divides in to two groups
+; then it creates a tree from left group and another tree from right group
+; and then it creates a new tree combining both of them.
+
+;	 	5
+;	1		9
+;	    3 	     7     11	
+
+
+; b) order of growth is n;
